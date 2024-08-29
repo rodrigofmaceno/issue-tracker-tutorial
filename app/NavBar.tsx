16d72e119748342e314a3a5f47bf6ac480b7080e
rodrigofmaceno <rferreiramaceno@gmail.com>
2024-08-29 17:13:10 -0600

    final commit

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import {AiFillBug} from "react-icons/ai"
+import classNames from "classnames"; // makes it easier to create conditional rendering for className
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  
   const links = [
     {label: "Dashboard", href: "/"},
     {label: "Issues", href: "/issues"}
@@ -10,16 +16,23 @@ const NavBar = () => {
 
 
   return ( 
-  <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/"><AiFillBug/></Link>
       <ul className="flex space-x-6">
-        {links.map((link) => 
-            <Link key={link.href} className="text-zinc-500 hover:text-zinc-800 transition-colors"  href={link.href}>{link.label}</Link>
-          )}
-          {/* <li><Link href="/" className="text-zinc-500 hover:text-zinc-800">Issues</Link></li> */}
+        {links.map(link => 
+          <Link 
+            key={link.href} 
+            // className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            // className={`${link.href === currentPath ? "text-zinc-900" : "text-zinc-500"} hover:text-zinc-800 transition-colors`}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}  
+            href={link.href}>{link.label}</Link>)}
       </ul>
-  </nav>
+    </nav>
   )
-};
+}
 
 export default NavBar;
